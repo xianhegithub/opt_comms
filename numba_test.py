6115d6423b1f6356a4e48e8e9d0f85f6977b2468
Xianhe Yangzhang <xianhe.yangzhang@outlook.com>
2020-08-29 11:28:11 +0100

    beginning of the day commit

--- numba_test.py
+++ numba_test.py
@@ -5,7 +5,7 @@ import time
 x = np.random.random([10, 10])
 
 @jit(nopython=True)
-def go_fast(a): # Function is compiled and runs in machine code
+def go_fast(a):  # Function is compiled and runs in machine code
     trace = 0.0
     for i in range(a.shape[0]):
         trace += np.tanh(a[i, :]).sum()
@@ -17,9 +17,8 @@ go_fast(x)
 end = time.time()
 print("Elapsed (with compilation) = %s" % (end - start))
 
-
 # NOW THE FUNCTION IS COMPILED, RE-TIME IT EXECUTING FROM CACHE
-for ii in range(4):
+for ii in range(10):
     start = time.time()
     go_fast(x)
     end = time.time()
